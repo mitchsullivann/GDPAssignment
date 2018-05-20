912231b6fb7b1e4fe7ea587e612330353ac507e7
Mitch Sullivan <s4912548@bournemouth.ac.uk>
2018-05-20 19:08:11 +0100

    firstcommit

---
+++ GDP ASS2/3DGDF/Assets/SCRIPT/SPINNY.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPINNY : MonoBehaviour
+{
+
+    public float speed = 200f;
+
+    // Update called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+
+
+	
+}
